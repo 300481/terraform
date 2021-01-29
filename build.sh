e81bd7f02d187a1be7099a3bccce760a98fd316e
300481 <300481@300481.de>
2021-01-29 17:09:20 +0100

    add build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+curl -s https://api.github.com/repos/hashicorp/terraform/tags?per_page=5 \
+| jq -r '.[].name' \
+| sed -e 's/^v//g' \
+| while read version ; do 
+    docker build \
+        --build-arg version=$version \
+        -t ghcr.io/300481/terraform:$version .
+done
