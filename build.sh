87fa0f8fe6fa1a38948f55d37ead8c470f17719b
Dennis Riemenschneider <300481@300481.de>
2021-02-01 15:04:00 +0000

    add handler for tagging

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+TAG_PATTERN="refs/tags/.*"
+
+if [[ ${GITHUB_REF} =~ $TAG_PATTERN ]] ; then
+    TAG=$(echo ${GITHUB_REF} | sed -e 's#refs/tags/##g')
+    echo Given tag: ${TAG}
+fi
+
+exit 0
+
 curl -s https://api.github.com/repos/hashicorp/terraform/tags?per_page=5 \
 | jq -r '.[].name' \
 | sed -e 's/^v//g' \
