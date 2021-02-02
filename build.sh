531b68e7eb7600bfb5144e5bdf62aa616c4afda9
Dennis Riemenschneider <300481@300481.de>
2021-02-02 15:17:15 +0000

    clean build.sh

--- build.sh
+++ build.sh
@@ -40,16 +40,3 @@ done
 
 exit 0
 
-
-if [[ ${GITHUB_REF} =~ $TAG_PATTERN ]] ; then
-    TAG=$(echo ${GITHUB_REF} | sed -e 's#refs/tags/##g')
-    echo Given git tag: ${TAG}
-    DOCKER_TAG=$(echo ${TAG} | sed -e 's#^v##g')
-
-    echo $GHTOKEN | docker login ghcr.io -u 300481 --password-stdin
-    docker build --build-arg version=${DOCKER_TAG} -t ghcr.io/300481/terraform:${DOCKER_TAG} .
-    docker push ghcr.io/300481/terraform:${DOCKER_TAG}
-fi
-
-exit 0
-
